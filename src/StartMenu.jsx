f0460e87bac324e5878fbce85aa3a727a0683e5c
JT <jstorbit@gmail.com>
2025-12-09 12:24:47 -0700

    Add base to StartMenu component

--- src/StartMenu.jsx
+++ src/StartMenu.jsx
@@ -1,5 +1,11 @@
 const StartMenu = () => {
-  return <h2>Welcome</h2>
+  return (
+    <section>
+      <h2>Welcome to Whack A Mole!</h2>
+      <p>You get a point for every mole you whack!</p>
+      <button>Play Now</button>
+    </section>
+  )
 }
 
 export default StartMenu;

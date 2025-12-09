376edc0fc702ca41e2a18b686c1b5b246a47a60c
JT <jstorbit@gmail.com>
2025-12-09 15:27:16 -0700

    Add isPlaying context to render welcome or board

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import StartMenu from "./StartMenu";
 import HighScore from "./HighScore";
 import ScoreBoard from "./ScoreBoard";
 import GameBoard from "./GameBoard";
+import { useGame } from "./GameContext";
 
 export default function App() {
-  return (
+  const { isPlaying } = useGame;
+  return (!isPlaying ?
     <>
       <h1>Whack A Mole!</h1>
       <StartMenu />
       <HighScore />
+    </> :
+    <>
+      <h1>Whack A Mole!</h1>
       <ScoreBoard />
       <GameBoard />
-    </>
-  );
+    </>)
 }

34009480a33cdb8acddbe864443cddb0b296a6d5
JT <jstorbit@gmail.com>
2025-12-10 07:28:20 -0700

    Add holes and mole to MoleHole component

--- src/GameBoard.jsx
+++ src/GameBoard.jsx
@@ -1,10 +1,11 @@
 import MoleHole from "./MoleHole";
-
+import { useGame } from "./GameContext";
   
 const GameBoard = () => {
+  const { numHoles } = useGame();
   return (
     <ul>
-      
+      {Array.from({length:numHoles}, (_, i) => <MoleHole key={i} index={i} />)}
     </ul>
   )
 }
--- src/MoleHole.jsx
+++ src/MoleHole.jsx
@@ -1,5 +1,10 @@
-const MoleHole = () => {
-  return <li></li>
+import { useGame } from "./GameContext";
+
+const MoleHole = ({index}) => {
+  const { moleHole } = useGame();
+  return index === moleHole ? 
+    <li className="hole mole"></li> :
+    <li className="hole"></li>
 }
 
 export default MoleHole;

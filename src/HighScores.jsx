567236ee8c414cc3ec00c9a6b997da3dfc9d40e0
JT <jstorbit@gmail.com>
2025-12-10 12:42:30 -0700

    Update backgrounds for more interactivity and clear UI navigation

--- src/HighScores.jsx
+++ src/HighScores.jsx
@@ -4,14 +4,15 @@ import { useGame } from "./GameContext";
 const HighScores = () => {
   const { highScores } = useGame();
   return highScores.length > 0 ? (
-    <section>
+    <section id="high-scores" className="start">
       <h2>High Scores:</h2>
       {highScores.map((score, i) => (
         <HighScore key={i} score={score} />
       ))}
     </section>
   ) : (
-    <section>
+    <section id="high-scores" className="start">
+      <h2>High Scores:</h2>
       <p>No scores yet</p>
     </section>
   );
--- src/StartMenu.jsx
+++ src/StartMenu.jsx
@@ -3,7 +3,7 @@ import { useGame } from "./GameContext";
 const StartMenu = () => {
   const { togglePlaying } = useGame();
   return (
-    <section>
+    <section id="menu" className="start">
       <h2>Welcome to Whack A Mole!</h2>
       <p>You get a point for every mole you whack!</p>
       <button onClick={togglePlaying}>Play Now</button>
